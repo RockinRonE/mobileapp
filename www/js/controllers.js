b8562bc5dd581b1d5da17cd4fbe04234283c2d92
System Administrator <root@Ronnys-MacBook-Air-2.local>
2016-08-08 08:29:46 -0500

    need to fix ionicModal

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,32 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['LocalStorageModule'])
 
-.controller('recentlyAddedCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.config(function (localStorageServiceProvider) {
+  localStorageServiceProvider
+    .setPrefix('names');
+})
+
+.controller('recentlyAddedCtrl', ['$scope', '$stateParams', '$ionicModal'  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+
+
+function ($scope, $stateParams, $ionicModal) {
+
   $scope.recentlyAdded = [
     { name: 'Jim', locationMet: 'Soho'},
     { name: 'Bob', locationMet: 'Michael'},
     { name: 'Sue', locationMet: 'Soho'}
   ];
+
+// configure ionic modal before use
+$ionicModal.fromTemplateUrl('new-connection-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(
+    (modal) => {
+      $scope.newTaskModal = modal;
+    }
+  );
 
 }])
 
